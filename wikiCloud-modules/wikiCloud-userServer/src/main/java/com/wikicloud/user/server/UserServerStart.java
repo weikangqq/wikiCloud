1b21205a69f82ff3f6677660b69e363f234fdbdc
weikang <J2Sks3wvsakc>
2020-10-10 17:41:42 +0800

    'sharding-jdbc'

--- wikiCloud-modules/wikiCloud-userServer/src/main/java/com/wikicloud/user/server/UserServerStart.java
+++ wikiCloud-modules/wikiCloud-userServer/src/main/java/com/wikicloud/user/server/UserServerStart.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSON;
 import com.wikicloud.base.server.TestMQ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,7 +16,6 @@ import java.util.function.Consumer;
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(value = {"com.wikicloud.*"})
-
 public class UserServerStart {
 
 
