1465bdd3db260a31f306137da9c5b1d7ff62acfc
weikang <J2Sks3wvsakc>
2020-10-10 15:13:10 +0800

    'nacos 配置文件'

--- wikiCloud-modules/wikiCloud-userServer/src/main/java/com/wikicloud/user/server/TestController.java
+++ wikiCloud-modules/wikiCloud-userServer/src/main/java/com/wikicloud/user/server/TestController.java
@@ -1,6 +1,5 @@
 package com.wikicloud.user.server;
 
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test")
 public class TestController {
 
-    @Value("${alipay.gateway_url}")
-    private String asdasd;
+    @Value("${a}")
+    private String a;
 
+    @Value("${b}")
+    private String b;
+
+    @Value("${c}")
+    private String c;
+
+    @Value("${d}")
+    private String d;
 
     @GetMapping("test1")
     public String test(){
 
-        return asdasd;
+        return d;
     }
 
 
