2ea066fa68ac67902df418fda26e1e054adc5d56
weikang <J2Sks3wvsakc>
2020-10-10 19:03:50 +0800

    'sharding-jdbc'

--- wikiCloud-Base/wikiCloud-base-shardingjdbc/src/main/java/com/wikicloud/base/shardingjdbc/config/MybatisPlusConfig.java
+++ wikiCloud-Base/wikiCloud-base-shardingjdbc/src/main/java/com/wikicloud/base/shardingjdbc/config/MybatisPlusConfig.java
@@ -5,26 +5,16 @@ import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.google.common.collect.Maps;
-import org.apache.ibatis.session.ExecutorType;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.Map;
 
 @EnableTransactionManagement
 @Configuration
 @MapperScan("com.wikicloud.user.server.mapper")
 public class MybatisPlusConfig {
-
-    private  MybatisPlusProperties properties;
+    
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
      */
